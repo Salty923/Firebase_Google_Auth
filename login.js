8a2a699a8804e9205282a2ec441f417c60081212
Scott Saltzman <salty923@gmail.com>
2018-01-16 18:05:27 -0600

    test1

--- login.js
+++ login.js
@@ -17,6 +17,19 @@ var provider = new firebase.auth.GoogleAuthProvider();
 
 var database = firebase.database();
 
+var user = firebase.auth().currentUser;
+var name, email, photoUrl, uid, emailVerified;
+
+if (user != null) {
+    name = user.displayName;
+    email = user.email;
+    photoUrl = user.photoURL;
+    emailVerified = user.emailVerified;
+    uid = user.uid;  // The user's ID, unique to the Firebase project. Do NOT use
+    // this value to authenticate with your backend server, if
+    // you have one. Use User.getToken() instead.
+}
+
 
 
 $("#signInBtn").on("click",function () {
@@ -42,7 +55,7 @@ $("#signInBtn").on("click",function () {
   });
 
   $("#submitBtn").on("click",function () {
-      database.ref().set("demo");
+      database.ref().set(user);
     })
   
 
