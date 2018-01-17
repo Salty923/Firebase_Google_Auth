bfb697bf164af6cc14a84a6c4c671b15096cc494
Scott Saltzman <salty923@gmail.com>
2018-01-16 22:39:09 -0600

    test11

--- login.js
+++ login.js
@@ -59,7 +59,7 @@ $("#signInBtn").on("click",function () {
             if (authData && isNewUser) {
                 // save the user's profile into Firebase so we can list users,
                 // use them in Security and Firebase Rules, and show profiles
-                database.child("users").child(authData.uid).set({
+                database.ref("users").child(authData.uid).set({
                     provider: authData.provider,
                     name: getName(authData)
                     //some more user data
